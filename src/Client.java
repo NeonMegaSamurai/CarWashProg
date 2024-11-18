0ceec7a975017e1979229b425423836b3249e757
MNShev <shevmn17@gmail.com>
2024-11-18 21:29:32 +0700

    Now we can create new client, but why?

---
+++ src/Client.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Client {
+
+    Scanner scanner = new Scanner(System.in);
+    String name;
+    String phoneNumber;
+    String carModel;
+    String licensePlates;
+
+    public void setNewClient(){
+        setName();
+        setPhoneNumber();
+        setCarModel();
+        setLicensePlates();
+    }
+    private void setName(){
+        System.out.print("Enter Client Name: ");
+        name = scanner.nextLine();
+    }
+    private void setPhoneNumber(){
+        System.out.print("Enter Client Phone Number: ");
+        phoneNumber = scanner.nextLine();
+    }
+    private void setCarModel(){
+        System.out.print("Enter Client Car Model: ");
+        carModel = scanner.nextLine();
+    }
+    private void setLicensePlates(){
+        System.out.print("Enter Client Car License Plates: ");
+        licensePlates = scanner.nextLine();
+    }
+    public void getClientInfo(){
+        getName(name);
+        getPhoneNumber(phoneNumber);
+        getCarModel(carModel);
+        getLicensePlates(licensePlates);
+    }
+    private void getName(String name){
+        this.name = name;
+        System.out.println("Client name: "+name);
+    }
+    private void getPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+        System.out.println("Client Phone Number: " + phoneNumber);
+    }
+    private void getCarModel(String carModel){
+        this.carModel = carModel;
+        System.out.println("Client Car Model: " + carModel);
+    }
+    private void getLicensePlates(String licensePlates){
+        this.licensePlates = licensePlates;
+        System.out.println("Client License Plates: " + licensePlates);
+    }
+}
