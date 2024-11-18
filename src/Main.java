0e19f15be10db44a025e03901c30dee8c4a66bef
MNShev <shevmn17@gmail.com>
2024-11-18 22:00:10 +0700

    Now we can create new client, but why?

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Client Bob = new Client();
-        Bob.setNewClient();
-        Bob.getClientInfo();
+        MainMenu menu = new MainMenu();
+        menu.mainMenuStart();
     }
 }
