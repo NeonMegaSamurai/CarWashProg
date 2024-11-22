5001c18ffc4c28cfa300edc5609c39f5b7592ca2
MNShev <shevmn17@gmail.com>
2024-11-22 21:13:46 +0700

    WTF? It's broken!

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -3,22 +3,35 @@ import java.util.Scanner;
 public class MainMenu {
     Scanner scanner = new Scanner(System.in);
 
-    Client [] nC = new Client[1];
-
     private byte choose;
     public void mainMenuStart(){
         while (true) {
+            int i = 0;
+            Client [] nC = new Client[i+1];
             System.out.println("Welcome again!" + "\n" + "1.Create new client"+ "\n" + "2.Clients information");
             choose = scanner.nextByte();
             if (choose == 1) {
                 Client client = new Client();
                 client.setNewClient();
-                nC[0] = client;
+                nC[i] = client;
             } else if (choose == 2) {
-                Client clientI = new Client();
-                clientI = nC[0];
-                clientI.getClientInfo();
-            }
+                System.out.println("Choose client:");
+                for (int a = 0; a<=nC.length; a++){
+                    System.out.println(a + ". " + nC[a].name + nC[a].carModel);
+                }
+                int chooseClient = scanner.nextInt();
+                for(int b = 0; b<=i; b++){
+                    if(chooseClient == b){
+                        nC[b].getClientInfo();
+                    }
+                }
+
+                if(nC[0] == null){
+                    System.out.println("You don't choose client!");
+                    continue;
+                }
+            } else {System.out.println("Please enter correct choice!");}
+            i = i++;
         }
     }
 }
